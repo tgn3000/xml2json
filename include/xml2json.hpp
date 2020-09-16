b8742909da392fe68ede4e475292a847e74ed049
tgn3000 <tgntanguangning@gmail.com>
2020-09-16 23:09:33 +0900

    fix cmake; compile with c++14; install library and header

--- include/xml2json.hpp
+++ include/xml2json.hpp
@@ -11,26 +11,6 @@
 #include <string>
 #include <cctype>
 
-#include "rapidxml/rapidxml.hpp"
-#include "rapidxml/rapidxml_utils.hpp"
-#include "rapidxml/rapidxml_print.hpp"
-
-#include "rapidjson/document.h"
-#include "rapidjson/prettywriter.h"
-#include "rapidjson/encodedstream.h"
-#include "rapidjson/stringbuffer.h"
-#include "rapidjson/reader.h"
-#include "rapidjson/writer.h"
-#include "rapidjson/filereadstream.h"
-#include "rapidjson/filewritestream.h"
-#include "rapidjson/error/en.h"
-
-// Avoided any namespace pollution.
-bool xml2json_has_digits_only(const char * input, bool *hasDecimal);
-void xml2json_to_array_form(const char *name, rapidjson::Value &jsvalue, rapidjson::Value &jsvalue_chd, rapidjson::Document::AllocatorType& allocator);
-void xml2json_add_attributes(rapidxml::xml_node<> *xmlnode, rapidjson::Value &jsvalue, rapidjson::Document::AllocatorType& allocator);
-void xml2json_traverse_node(rapidxml::xml_node<> *xmlnode, rapidjson::Value &jsvalue, rapidjson::Document::AllocatorType& allocator);
-
 XML2JSON_API std::string xml2json(const char *xml_str);
 
 #endif
